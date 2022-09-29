ba2b88fbbb8a6749cb35ac56894fb7da87334c31
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-30 03:09:45 +1000

    add fields to PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -12,4 +12,11 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
-
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    appeared_at = models.DateTimeField(auto_now_add=True)
+    disappeared_at = models.DateTimeField(null=True)
+    level = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
+    damage = models.IntegerField(default=0)
+    protection = models.IntegerField(default=0)
+    stamina = models.IntegerField(default=0)
