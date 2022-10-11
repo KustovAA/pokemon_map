12daa2388cd15f8cb14754c9331d43fc0d0a53b4
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-10-11 16:15:45 +1000

    remove dangerous default values

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -26,11 +26,11 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name='Покемон')
     appeared_at = models.DateTimeField(verbose_name='Время появления')
     disappeared_at = models.DateTimeField(verbose_name='Время исчезновения')
-    level = models.IntegerField(default=1, verbose_name='Уровень')
-    health = models.IntegerField(default=1, verbose_name='Здоровье')
-    damage = models.IntegerField(default=1, verbose_name='Урон')
-    protection = models.IntegerField(default=1, verbose_name='Защита')
-    stamina = models.IntegerField(default=1, verbose_name='Выносливость')
+    level = models.IntegerField(verbose_name='Уровень')
+    health = models.IntegerField(verbose_name='Здоровье')
+    damage = models.IntegerField(verbose_name='Урон')
+    protection = models.IntegerField(verbose_name='Защита')
+    stamina = models.IntegerField(verbose_name='Выносливость')
 
     def __str__(self):
         return f'Entity {self.id} for {self.pokemon.title}'
